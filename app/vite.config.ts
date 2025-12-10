903c631cdbe7a563348a1f1818f3f41bca50e227
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2025-12-10 17:45:50 +0100

    init

---
+++ app/vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'IOT-WOL',
+      short_name: 'IOT-WOL',
+      description: 'An app that lets you turn on computers remotely with Wake-On-Lan using IOT and MQTT',
+      theme_color: '#39CE8E',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
