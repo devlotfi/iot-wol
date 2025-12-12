bdbf187bb7fbc58aa3a2e6d003617762c15128c8
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2025-12-12 21:49:39 +0100

    update

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -18,26 +18,24 @@ function RootComponent() {
 
   return (
     <React.Fragment>
-      <div className="flex flex-col md:flex-row min-h-dvh min-w-dvw max-h-dvh max-w-dvw overflow-hidden bg-content1">
-        {isLoading ? (
-          <Progress
-            isIndeterminate
-            aria-label="Loading..."
-            size="sm"
-            className="fixed w-dvw z-50"
-          />
-        ) : null}
+      {isLoading ? (
+        <Progress
+          isIndeterminate
+          aria-label="Loading..."
+          size="sm"
+          className="fixed w-dvw z-50"
+        />
+      ) : null}
 
-        <Sidebar></Sidebar>
-        <ScrollShadow
-          visibility="bottom"
-          className="flex flex-col flex-1 h-[calc(100dvh-5rem)] md:h-auto md:w-[calc(100dvw-6rem)] overflow-x-hidden overflow-y-auto bg-[linear-gradient(to_top,hsl(var(--heroui-content1)/1)_0%,hsl(var(--heroui-content2)/1)_10%,hsl(var(--heroui-content2)/1)_100%)] md:bg-[linear-gradient(to_right,hsl(var(--heroui-content1)/1)_0%,hsl(var(--heroui-content2)/1)_10%,hsl(var(--heroui-content2)/1)_100%)]"
-        >
-          <ConnectionStatus></ConnectionStatus>
-          <Outlet />
-        </ScrollShadow>
-        <BottomTabs></BottomTabs>
-      </div>
+      <Sidebar></Sidebar>
+      <ScrollShadow
+        visibility="bottom"
+        className="flex flex-col flex-1 h-[calc(100dvh-5rem)] md:h-auto md:w-[calc(100dvw-6rem)] overflow-x-hidden overflow-y-auto bg-[linear-gradient(to_top,hsl(var(--heroui-content1)/1)_0%,hsl(var(--heroui-content2)/1)_10%,hsl(var(--heroui-content2)/1)_100%)] md:bg-[linear-gradient(to_right,hsl(var(--heroui-content1)/1)_0%,hsl(var(--heroui-content2)/1)_10%,hsl(var(--heroui-content2)/1)_100%)]"
+      >
+        <ConnectionStatus></ConnectionStatus>
+        <Outlet />
+      </ScrollShadow>
+      <BottomTabs></BottomTabs>
     </React.Fragment>
   );
 }
